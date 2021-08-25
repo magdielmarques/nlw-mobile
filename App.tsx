2d395e39ee711b22b90cbb31378a1724f3cbcecf
Magdiel09 <magdiel092013@gmail.com>
2021-08-25 15:51:38 -0400

    42:00 min

--- App.tsx
+++ App.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { App } from 'expo'
 import { StatusBar } from 'react-native';
 
+import AppLoading from 'expo-app-loading';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
 import {Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
 
 import Home from './src/pages/Home';
 
 export default function App() {
-  const [fontsLoaded] =  useFonts({
+  const [fonstLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_500Medium,
     Ubuntu_700Bold
   })
 
-  if(!fontsLoaded){
-    return 
+  if (!fonstLoaded) {
+    return <AppLoading />
   }
 
   return (
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
-import { View, Image, StyleSheet } from 'react-native'
+import { View, ImageBackground, Image, StyleSheet, Text } from 'react-native'
 
 const Home = () => {
     return (
-        <View style={styles.container}>
-            <Image source={require('../../assets/logo.png')} />
-        </View>
+        <ImageBackground source={require('../../assets/home-background.png')} 
+            style={styles.container}
+            imageStyle={{ width: 274, height: 368 }}
+        >
+            <View style={styles.main}>
+                <Image source={require('../../assets/logo.png')} />
+                <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+                <Text style={styles.description}>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</Text>
+                
+            </View>
+
+            <View style={styles.footer}>
+                
+            </View>
+
+
+        </ImageBackground>
     )
 }
 
@@ -13,6 +27,7 @@ const styles = StyleSheet.create({
 container: {
     flex: 1,
     padding: 32,
+    backgroundColor: '#f0f0f5'
 },
 
 main: {
