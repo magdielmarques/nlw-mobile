58da7b182b22c20411b1c5cb1a0c08b3f1184b2f
magdielmarques <contato.magdiel@gmail.com>
2021-09-09 14:05:51 -0400

    Finalizando buscando--itens-da-api

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -14,6 +14,7 @@ interface Item {
 
 const Points = () => {
     const[items, setItems] = useState<Item[]>([]);
+    const[selectedItems, setSelectedItems] = useState<number[]>([]);
     const navigation = useNavigation<any>()
 
     useEffect(() => {
@@ -28,6 +29,17 @@ const Points = () => {
     
     function handleNavigaToDetail() {
         navigation.navigate('Detail');
+    }
+
+    function handleSelectItem(id: number){ 
+        const alreadySelected = selectedItems.findIndex(item => item === id);
+
+        if(alreadySelected >= 0){
+            const filteredItems = selectedItems.filter(item => item !== id);
+            setSelectedItems(filteredItems)
+        } else {
+            setSelectedItems([...selectedItems, id]);
+        }
     }
 
     return (
@@ -73,14 +85,22 @@ const Points = () => {
                     contentContainerStyle={{ paddingHorizontal: 20 }}
                 >
                     {items.map(item => (
-                        <TouchableOpacity key={String(item.id)} style={styles.item} onPress={() => {}}>
+                        <TouchableOpacity 
+                            key={String(item.id)} 
+                            style={[
+                                styles.item,
+                                selectedItems.includes(item.id) ? styles.selectedItem : {}
+                            ]} 
+                            onPress={() => handleSelectItem(item.id)}
+                            activeOpacity={0.6}
+                        >
                             <SvgUri width={42} height={42} uri={item.image_url} />
                             <Text style={styles.itemTitle}>{item.title}</Text>
                         </TouchableOpacity>
                     ))}
                 </ScrollView>
             </View>
-            </SafeAreaView>
+        </SafeAreaView>
     )
 };
 
