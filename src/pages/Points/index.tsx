ff270ca0383aaa628672be1edfbb0eacc52e3ec5
Magdiel Marques <40240561+magdielmarques@users.noreply.github.com>
2021-09-14 14:28:07 -0400

    Revert "Finalizando projeot v2"

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -17,7 +17,6 @@ interface Point {
     id: number;
     name: string;
     image: string;
-    image_url: string;
     latitude: number;
     longitude: number;
 }
@@ -129,7 +128,7 @@ const Points = () => {
                                 }}
                             >
                                 <View style={styles.mapMarkerContainer}>
-                                    <Image style={styles.mapMarkerImage} source={{ uri: point.image_url }} />
+                                    <Image style={styles.mapMarkerImage} source={{ uri: point.image }} />
                                     <Text style={styles.mapMarkerTitle}> {point.name} </Text>
                                 </View>
                             </Marker>
